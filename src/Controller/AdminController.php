300cb5798ff1fc106a08d16c141162e65f77e46f
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2024-01-14 11:51:44 +0600

    AdminController Created

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class AdminController extends AbstractController
 {
@@ -14,6 +16,24 @@ class AdminController extends AbstractController
     public function index(): Response
     {
         return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+    public function edit(Product $product, UserInterface $user): Response
+    {
+        // Check if the current user is allowed to edit this product
+        if (!$this->isGranted('EDIT', $product)) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $form = $this->createForm(ProductType::class, $product);
+
+        return $this->render('admin/edit.html.twig', [
+            'form' => $form->createView(),
+            'product' => $product,
+            'user' => $user,
+            'csrf_protection' => true,
             'controller_name' => 'AdminController',
         ]);
     }
